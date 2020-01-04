d1bff88321dd66b51f37184382019458e26bb422
Joakim-dev <jaydev-sweden@gmail.com>
2020-01-04 15:05:47 +0100

    changes for responsiveness

--- js/navbar.js
+++ js/navbar.js
@@ -1,24 +1,6 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.menu');
-    const navLinks = document.querySelectorAll('.menu li');
+const toggleButton = document.getElementsByClassName('toggle-button')[0];
+const navbarLinks = document.getElementsByClassName('navbar-links')[0];
 
-    
-    burger.addEventListener('click', ()=>{
-        //Toggle Nav
-        nav.classList.toggle('nav-active');
-
-        // Animate Liks
-        navLinks.forEach((link, index) => {
-        if (link.style.animation){
-            link.style.animation = '';
-        } else {
-            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle('toggle');
-    });
-}
-
-navSlide();
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+});
